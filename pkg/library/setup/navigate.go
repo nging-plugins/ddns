be9b595ca719d4a5c974125e8ed791570a2283de
Hank Shen <swh@admpub.com>
2021-12-15 15:53:54 +0800

    update

--- pkg/library/setup/navigate.go
+++ pkg/library/setup/navigate.go
@@ -1,12 +1,12 @@
 package setup
 
 import (
-	"github.com/admpub/nging/v4/application/handler/tool"
+	"github.com/admpub/nging/v4/application/handler/manager"
 	"github.com/admpub/nging/v4/application/registry/navigate"
 )
 
 func init() {
-	tool.TopNavigate.AddChild(`tool`, -1, &navigate.Item{
+	manager.TopNavigate.AddChild(`tool`, -1, &navigate.Item{
 		Display: true,
 		Name:    `DDNS`,
 		Action:  `ddns`,
