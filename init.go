d3f7847e1e174e31dede4351be500d8b9678dd80
Hank Shen <swh@admpub.com>
2021-12-18 12:39:51 +0800

    update

--- init.go
+++ init.go
@@ -1,10 +1,5 @@
 package ddnsmanager
 
-import (
-	_ "github.com/nging-plugins/ddnsmanager/pkg/handler"
-	_ "github.com/nging-plugins/ddnsmanager/pkg/library/setup"
-)
-
 func init() {
 
 }
---
+++ module.go
@@ -0,0 +1,18 @@
+package ddnsmanager
+
+import (
+	"github.com/admpub/nging/v4/application/library/module"
+
+	"github.com/nging-plugins/ddnsmanager/pkg/handler"
+)
+
+const ID = `ddns`
+
+var Module = module.Module{
+	TemplatePath: map[string]string{
+		ID: `ddnsmanager/template/backend`,
+	},
+	AssetsPath: []string{},
+	Navigate:   RegisterNavigate,
+	Route:      handler.RegisterRoute,
+}
