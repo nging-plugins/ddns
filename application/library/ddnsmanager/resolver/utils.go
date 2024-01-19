853bc347bb790d84a5ce386383791133d8ddbac1
Hank Shen <swh@admpub.com>
2024-01-19 19:56:44 +0800

    fix

--- application/library/ddnsmanager/resolver/utils.go
+++ application/library/ddnsmanager/resolver/utils.go
@@ -5,7 +5,13 @@ import (
 	"strings"
 
 	"github.com/miekg/dns"
+	"github.com/webx-top/echo/param"
 )
+
+func trimSpaceAndNotEmpty(s *string) bool {
+	*s = strings.TrimSpace(*s)
+	return len(*s) > 0
+}
 
 // ResolveDNS will query DNS for a given hostname.
 // example: ResolveDNS(`webx.top`,`8.8.8.8`,`IPV4`)
@@ -16,9 +22,13 @@ func ResolveDNS(hostname, resolver, ipType string) (string, error) {
 	} else {
 		dnsType = dns.TypeAAAA
 	}
+	var resolvers []string
+	if len(resolver) > 0 {
+		resolvers = param.Split(resolver, `,`).Filter(trimSpaceAndNotEmpty).Unique().String()
+	}
 
 	// If no DNS server is set in config file, falls back to default resolver.
-	if len(resolver) == 0 {
+	if len(resolvers) == 0 {
 		dnsAdress, err := net.LookupHost(hostname)
 		if err != nil {
 			return "", err
@@ -26,7 +36,7 @@ func ResolveDNS(hostname, resolver, ipType string) (string, error) {
 
 		return dnsAdress[0], nil
 	}
-	res := New([]string{resolver})
+	res := New(resolvers)
 	// In case of i/o timeout
 	res.RetryTimes = 5
 
